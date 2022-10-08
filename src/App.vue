d13a0b3daf39700e986f0030083a19ed89ca7e14
DZ8540 <d.z.2021@inbox.ru>
2022-10-08 20:29:08 +0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <span class="color__black font__title">Its worked!</span>
+</template>
+
+<style scoped></style>
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import App from './App.vue'
+import { createApp } from 'vue'
+
+import './assets/css/main.css'
+
+createApp(App).mount('#app')
