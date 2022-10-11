897e690a1e8f3c062fa10f52e127f11b4e23eb4e
DZ8540 <d.z.2021@inbox.ru>
2022-10-11 09:54:16 +0600

    Feature: Added environments

--- src/api/httpClient.ts
+++ src/api/httpClient.ts
@@ -1,11 +1,12 @@
 // * Types
+import { API_URL } from '@/config/api'
 import type { AxiosInstance } from 'axios'
 // * Types
 
 import axios from 'axios'
 
 export const HTTP_CLIENT: AxiosInstance = axios.create({
-  baseURL: 'https://hookb.in',
+  baseURL: API_URL,
   timeout: 1000,
   headers: { 
     'Content-Type': 'application/json',
--- src/config/api.ts
+++ src/config/api.ts
@@ -1,3 +1,5 @@
+export const API_URL: string = import.meta.env.VITE_API_URL ?? 'https://google.com'
+
 export const API_ROUTES = {
   calcSendData: '/eK160jgYJ6UlaRPldJ1P'
 } as const
